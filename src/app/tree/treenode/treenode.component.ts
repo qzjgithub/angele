578546c3d4c583bb09961730b0488eca2a8864f2
qzjgithub <qzhujun@sina.com>
2017-07-11 17:39:15 +0800

    tree

--- src/app/tree/treenode/treenode.component.ts
+++ src/app/tree/treenode/treenode.component.ts
@@ -1,23 +1,90 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {
+  Component, OnInit, Input, AfterViewInit, EmbeddedViewRef, ViewChild, TemplateRef,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'treenode',
   templateUrl: './treenode.component.html',
   styleUrls: ['./treenode.component.css']
 })
-export class TreenodeComponent implements OnInit {
+export class TreenodeComponent implements OnInit, AfterViewInit {
 
   @Input()
-  node: Object;
+  node : Object;
+
+  @ViewChild('children')
+  ul : ElementRef;
+
+  /**
+   * 小三角表示的展示和隐藏
+   */
+  arrowActive:boolean;
+
+  /**
+   * 是否在动画中
+   */
+  animate:boolean;
 
   constructor() {
+    this.arrowActive = false;
+    this.animate = false;
   }
 
   ngOnInit() {
+    this.arrowActive = this.node['unfold'];
   }
 
-  troggleFold(node){
-    node.unfold = !node.unfold;
+  ngAfterViewInit(): void {
+    if(this.ul){
+      let uldom = this.ul.nativeElement;
+      let len = this.calNode(this.node);
+      uldom.style.height = 'auto';
+    }
+  }
+
+  troggleFold(event, node){
+    // node.unfold = !node.unfold;
+    let i = event.target;
+    if(this.animate || !i) return;
+    this.animate = true;
+    this.arrowActive = !this.arrowActive;
+    if(node.unfold){
+      let ul = i.parentElement.parentElement.nextElementSibling;
+      ul.style.height = ul.offsetHeight + 'px';
+      setTimeout(() => {
+        ul.style.height = 0 + 'px';
+      },10);
+      setTimeout(() => {
+        node.unfold = !node.unfold;
+        this.animate = false;
+      },510);
+    }else{
+      node.unfold = !node.unflod;
+      let len = this.calNode(node);
+      setTimeout(() => {
+        let ul = i.parentElement.parentElement.nextElementSibling;
+        ul.style.height = 30 * len + 'px'
+      },10);
+      setTimeout(() => {
+        let ul = i.parentElement.parentElement.nextElementSibling;
+        this.animate = false;
+        ul.style.height = 'auto'
+      },510);
+    }
+    event.stopPropagation();
+  }
+
+  calNode(node){
+    let num = 0;
+    node.children &&
+    node.children.length &&
+    node.unfold &&
+    node.children.forEach((e,i) => {
+      num++;
+      num += this.calNode(e);
+    });
+    return num;
   }
 
 }
