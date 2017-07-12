ce7ee0c7440f4ceb96583bda5ab558b172d7d061
qzjgithub <qzhujun@sina.com>
2017-07-12 17:47:44 +0800

    stylus

--- src/app/tree/treenode/treenode.component.ts
+++ src/app/tree/treenode/treenode.component.ts
@@ -27,54 +27,82 @@ export class TreenodeComponent implements OnInit, AfterViewInit {
   animate:boolean;
 
   constructor() {
+    //默认箭头关闭
     this.arrowActive = false;
+    //默认不处于动画中
     this.animate = false;
   }
 
   ngOnInit() {
+    //设置箭头的展开状态
     this.arrowActive = this.node['unfold'];
   }
 
   ngAfterViewInit(): void {
+    //如果默认是展开的，则获取到ul，则将ul的高度置为自动
     if(this.ul){
       let uldom = this.ul.nativeElement;
-      let len = this.calNode(this.node);
       uldom.style.height = 'auto';
     }
   }
 
+  /**
+   * 打开和关闭树节点
+   * @param event
+   * @param node
+   */
   troggleFold(event, node){
-    // node.unfold = !node.unfold;
+    //获取点击对象
     let i = event.target;
+    //若处于动画中或没有获取到则返回
     if(this.animate || !i) return;
+    //设置处于动画中
     this.animate = true;
+    //改变箭头状态
     this.arrowActive = !this.arrowActive;
-    if(node.unfold){
+
+    if(node.unfold){//如果是打开的则关闭
+      //获取子节点容器
       let ul = i.parentElement.parentElement.nextElementSibling;
+      //为ul的height样式设置实际的值
       ul.style.height = ul.offsetHeight + 'px';
       setTimeout(() => {
+        //关闭子节点
         ul.style.height = 0 + 'px';
       },10);
       setTimeout(() => {
+        //改变树节点为关闭状态
         node.unfold = !node.unfold;
+        //设置不处于动画中
         this.animate = false;
       },510);
-    }else{
-      node.unfold = !node.unflod;
+    }else{//如果是关闭的则打开
+      node.unfold = !node.unfold;
+      //得到需要设置的子节点个数
       let len = this.calNode(node);
       setTimeout(() => {
+        //获取子节点容器
         let ul = i.parentElement.parentElement.nextElementSibling;
-        ul.style.height = 30 * len + 'px'
+        //如果获取到则改变高度
+        if(ul) ul.style.height = 30 * len + 'px'
       },10);
       setTimeout(() => {
+        //获取子节点容器
         let ul = i.parentElement.parentElement.nextElementSibling;
+        //设置不处于动画中
         this.animate = false;
+        //设置子节点高度为自动
         ul.style.height = 'auto'
       },510);
     }
     event.stopPropagation();
   }
 
+  /**
+   * 计算某树节点如果展开的话，需要展示的子节点个数
+   * @param node
+   * @returns {number}
+   */
   calNode(node){
     let num = 0;
     node.children &&
